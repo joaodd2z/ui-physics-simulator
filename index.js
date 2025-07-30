020e2a4c94bb520631c443c1012798495167f7fd
joao lucas De Oliveira <134670775+joaodd2z@users.noreply.github.com>
2025-07-30 08:31:20 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// Este arquivo garante que o Vercel reconheça o projeto como um site estático
+// O arquivo principal é index.html
+console.log('UI Physics Simulator - Static Site');
+
+// Exporta uma função vazia para compatibilidade
+module.exports = () => {
+    return 'Static site - see index.html';
+};
